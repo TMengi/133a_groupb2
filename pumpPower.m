cba400ef067cd9741b3ed635ca39f2a31c20b66b
jmgardiner1 <61670179+jmgardiner1@users.noreply.github.com>
2020-03-09 10:39:48 -0700

    Add files via upload
    calculates the pump power based on the pressures, fluid properties, and blade geometry.

---
+++ pumpPower.m
@@ -0,0 +1,19 @@
+
+%%This function claculates the pump power based on the equations given on
+%%page 287 of Mechanics and Thermdynamics of Propulsion
+
+%mdot = mass flow rate of the fluid
+%Ti = the initial temperature at the entrance of the pump
+%efficiency = efficiency of the pump in decimal
+%Pi = initial pressure entering pump
+%Pf = final pressure leaving the pump
+%gamma = heat capacity ratio of the fluid
+%cp = specific heat at constant pressure
+%ctheta1 = tangential velocity of fluid entiring rotor
+%ctheta2 = tangential velocity of fluid exiting rotor
+
+function [pumpPower] = pumpPower(mdot,Ti,efficiency,Pi,Pf,gamma,cp,ctheta1,ctheta2)
+    deltaTo = (Ti/efficiency)*((Pf/Pi)^((gamma-1)/gamma) - 1);
+    U = (deltaTo*cp)/(ctheta2-ctheta1);
+    pumpPower = U*mdot*(ctheta1-ctheta2);
+end
