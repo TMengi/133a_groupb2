038688c4db97c46b824018bc590ab2f791942111
jmgardiner1 <61670179+jmgardiner1@users.noreply.github.com>
2020-03-09 20:09:16 -0700

    Update pumpPower.m

--- pumpPower.m
+++ pumpPower.m
@@ -1,9 +1,13 @@
-%%This function claculates the pump power based on the bernoulli equation
-%mdot = mass flow rate of the fluid
-%density
-%efficiency = efficiency of the pump in decimal
-%Pi = initial pressure entering pump
-%Pe = final pressure leaving the pump
+%%This function calculates the pump power based on the bernoulli equation.
+%INPUTS
+%mdot = mass flow rate of the working fluid (kg/s)
+%density = density of the working fluid (kg/m^3)
+%efficiency = efficiency of the pump (decimal value)
+%Pi = initial pressure entering pump (Pascal)
+%Pe = final pressure leaving the pump (Pascal)
+
+%OUTPUTS
+%pumpP = the power required to drive pump (Watts)
 function [pumpP] = pumpPower(mdot,density,efficiency,Pe,Pi)
     if (efficiency > 1) || (efficiency < 0)
        error('The efficiency should be a decimal value from 0 to 1');
