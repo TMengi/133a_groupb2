0addcdd587c48bfbc08cabbc172a794f14b88be5
jmgardiner1 <61670179+jmgardiner1@users.noreply.github.com>
2020-03-09 20:02:55 -0700

    Update pumpPower.m

--- pumpPower.m
+++ pumpPower.m
@@ -1,19 +1,12 @@
-
-%%This function claculates the pump power based on the equations given on
-%%page 287 of Mechanics and Thermdynamics of Propulsion
-
+%%This function claculates the pump power based on the bernoulli equation
 %mdot = mass flow rate of the fluid
-%Ti = the initial temperature at the entrance of the pump
+%density
 %efficiency = efficiency of the pump in decimal
 %Pi = initial pressure entering pump
-%Pf = final pressure leaving the pump
-%gamma = heat capacity ratio of the fluid
-%cp = specific heat at constant pressure
-%ctheta1 = tangential velocity of fluid entering rotor
-%ctheta2 = tangential velocity of fluid exiting rotor
-
-function [pumpPower] = pumpPower(mdot,Ti,efficiency,Pi,Pf,gamma,cp,ctheta1,ctheta2)
-    deltaTo = (Ti/efficiency)*((Pf/Pi)^((gamma-1)/gamma) - 1);
-    U = (deltaTo*cp)/(ctheta2-ctheta1);
-    pumpPower = U*mdot*(ctheta1-ctheta2);
+%Pe = final pressure leaving the pump
+function [pumpP] = pumpPower(mdot,density,efficiency,Pe,Pi)
+    if (efficiency > 1) || (efficiency < 0)
+       error('The efficiency should be a decimal value from 0 to 1');
+    end
+    pumpP = (mdot*(Pe-Pi))/(density*efficiency);
 end
